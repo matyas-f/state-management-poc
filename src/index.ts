c73052ab593ea38a2b2faf646487af4f398a49ca
Mátyás Fürtös <matyas.furtos@gmail.com>
2023-03-27 17:39:37 +0300

    feat(sum3): added another sum fn
    Adds 3 numbers together

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ export type Test1 = {
   num2: number;
 };
 
+export const sum1 = (num1: number, num2: number) => num1 + num2;
+
 export const sum2 = (args: Test1) => args.num1 + args.num2;
 
-export const sum1 = (num1: number, num2: number) => num1 + num2;
+export const sum3 = (num1: number, num2: number, num3: number) =>
+  num1 + num2 + num3;
