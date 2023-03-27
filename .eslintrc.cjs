3580b2df44ec890be55ef394f751029582963966
Mátyás Fürtös <matyas.furtos@gmail.com>
2023-03-27 19:34:17 +0300

    feat(docs): added docusaurus docs
    Basic template

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
